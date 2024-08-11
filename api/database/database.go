e27ab930f677c91c6f21a6ad5fdd8595c5739f72
0mwa <omwaoqwa@gmail.com>
2024-08-11 21:38:01 +0300

    add .env & Redis init

--- api/database/database.go
+++ api/database/database.go
@@ -1,1 +1,15 @@
 package database
+
+import (
+	"github.com/go-redis/redis/v8"
+	"os"
+)
+
+func NewRedisClient(dbNo int) *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_URL"),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB:       dbNo,
+	})
+	return rdb
+}
